f39b0804400dce959fbfbac69cff3b52f30146ea
Stephanie Hiyagon <mv.hiyagon@gmail.com>
2017-05-23 10:11:04 -0500

    aside-car

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
-$(document).foundation()
+// $(document).foundation()
+
+//carrito
+
+var aside=document.getElementById('asideCar');
+var car=document.getElementById('carBag');
+var carActivo=document.getElementsByClassName('car')[0];
+var cerrar=document.getElementById('close-cart');
+var overlay=document.getElementsByClassName('asideActive')[0];
+var more=document.getElementsByClassName('shopMore')[0];
+
+function compra(event){
+  aside.style.display="block";
+  carActivo.classList.add("carActivo");
+}
+
+function cierra(event){
+  aside.style.display="none";
+  carActivo.classList.remove("carActivo");
+}
+
+cerrar.addEventListener('click',cierra);
+car.addEventListener('click', compra);
+overlay.addEventListener('click',cierra);
+more.addEventListener('click',cierra);
